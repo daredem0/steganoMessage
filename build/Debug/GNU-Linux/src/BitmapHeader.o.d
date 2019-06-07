2e6f9e00f0ce64ef720f6ca9a97c05fb08b96c8d
Tobias Wirth <tobiaswirth@Tobias-MacBook-Pro.local>
2019-06-07 19:52:35 +0200

    For testing purposes

--- build/Debug/GNU-Linux/src/BitmapHeader.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/GNU-Linux/src/BitmapHeader.o: src/BitmapHeader.cpp \
- src/../header/BitmapHeader.h src/../header/./ErrorHandler.h \
- src/../header/./constants.h
-
-src/../header/BitmapHeader.h:
-
-src/../header/./ErrorHandler.h:
-
-src/../header/./constants.h:
