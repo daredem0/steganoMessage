200b36d4def94f0882266382d548e5706224069c
st155013 <st155013@stud.uni-stuttgart.de>
2019-06-16 13:18:30 +0200

    added cleanup script; fixed install target; cleaned up

--- build/Debug/GNU-Linux/src/BitmapArray.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/GNU-Linux/src/BitmapArray.o: src/BitmapArray.cpp \
- src/../header/BitmapArray.h src/../header/./ErrorHandler.h \
- src/../header/./constants.h
-
-src/../header/BitmapArray.h:
-
-src/../header/./ErrorHandler.h:
-
-src/../header/./constants.h:
