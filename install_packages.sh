c9062b4ee3011b528fa92eea86e53a54847a22c9
dare.demo <f.leuze@outlook.de>
2022-04-17 10:04:27 +0200

    build: added script which installs necessary packages on a linux system

---
+++ install_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt install -y build-essential apt-utils gcc make cmake binutils
+
+sudo apt install -y libjpeg-turbo8-dev zlib1g-dev libpng-dev libglew-dev libsfml-dev libglfw3-dev libglm-dev 
